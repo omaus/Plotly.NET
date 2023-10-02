769047daf965673d89d5367d6ddb969362f7b149
Oliver Maus <oliver.maus@eclipso.de>
2023-10-02 15:03:26 +0200

    Update prototype of ProportionalAreaChart

--- src/Plotly.NET/Playground.fsx
+++ src/Plotly.NET/Playground.fsx
@@ -30,11 +30,19 @@ Chart.Bubble(xysizes, MarkerSymbol = StyleParam.MarkerSymbol.CircleCross, Orient
 
 type Chart with
 
-    static member ProportionalArea(posGroupSizes: (#System.IConvertible * #System.IConvertible * int) list, ?Orientation: StyleParam.Orientation, ?MarkerSymbol: StyleParam.MarkerSymbol, ?MarkerColor: Color, ?MarkerColorScale: StyleParam.Colorscale) =
+    static member ProportionalArea(factorGroupSizes: (#System.IConvertible * #System.IConvertible * int) list, ?Orientation : StyleParam.Orientation, ?MarkerSymbol : StyleParam.MarkerSymbol, ?MarkerColor : Color, ?MarkerColorScale : StyleParam.Colorscale) =
 
-        let biggestDiameter = posGroupSizes |> List.maxBy (fun (p,g,s) -> s) |> fun (p,g,s) -> float s
-        let smallestDiameter = posGroupSizes |> List.minBy (fun (p,g,s) -> s) |> fun (p,g,s) -> float s
-        let sizeAdjusted = posGroupSizes |> List.map (fun (p,g,s) -> p, g, (float s + 3. - smallestDiameter) / (biggestDiameter + 3. - smallestDiameter) * 175. |> int)
+        let biggestDiameter = factorGroupSizes |> List.maxBy (fun (p,g,s) -> s) |> fun (p,g,s) -> float s
+        let smallestDiameter = factorGroupSizes |> List.minBy (fun (p,g,s) -> s) |> fun (p,g,s) -> float s
+        let deltaBiggestSmallest = match biggestDiameter - smallestDiameter with | 0. -> 1. | _ -> biggestDiameter - smallestDiameter
+        let sizeAdjusted =      // size shall range from 3 px (lowest) to 175 px (highest)
+            factorGroupSizes 
+            |> List.map (
+                fun (p,g,s) -> 
+                    p, 
+                    g, 
+                    (float s - smallestDiameter) / deltaBiggestSmallest * 172. + 3. |> round |> int
+            )
 
         // use categorical axis for y axis description (maybe via casting to string)
 
@@ -42,15 +50,20 @@ type Chart with
         | None
         | Some StyleParam.Orientation.Horizontal ->
             Chart.Bubble(sizeAdjusted, ?MarkerSymbol = MarkerSymbol, ?MarkerColor = MarkerColor, ?MarkerColorScale = MarkerColorScale)
+            |> Chart.withYAxis(LayoutObjects.LinearAxis.init (AutoRange = StyleParam.AutoRange.Reversed))
         | Some StyleParam.Orientation.Vertical ->
             sizeAdjusted 
             |> List.map (fun (p,g,s) -> g, p, s)
             |> fun orientationAdjusted -> Chart.Bubble(orientationAdjusted, ?MarkerSymbol = MarkerSymbol) 
-            |> Chart.withYAxis(LayoutObjects.LinearAxis.init (AutoRange = StyleParam.AutoRange.Reversed))     // search for a way to reverse y axis but without changing the values since they could be strings
+            |> Chart.withYAxis(LayoutObjects.LinearAxis.init (AutoRange = StyleParam.AutoRange.Reversed))
+
+    static member PercentageBlocksChart(groupPercentages : (#System.IConvertible * int) list, ?Orientation : StyleParam.Orientation, ?MarkerColor : Color) =
+        0
 
 
 Chart.ProportionalArea(xysizes, MarkerColor = Color.fromColors [Color.fromString "black"; Color.fromString "red"]) |> Chart.show
-Chart.ProportionalArea(xysizes, MarkerColorScales = [StyleParam.Colorscale.Electric]) |> Chart.show
+Chart.ProportionalArea(xysizes, MarkerColor = Color.fromColors [Color.fromString "black"; Color.fromString "red"]) |> Chart.withSize(1800.,800.) |> Chart.show
+Chart.ProportionalArea(xysizes, MarkerColorScale = StyleParam.Colorscale.Electric) |> Chart.show
 Chart.ProportionalArea(xysizes, Orientation = StyleParam.Orientation.Vertical) |> Chart.show
 
 Chart.Line([1,1; 2,2], LineColorScale = StyleParam.Colorscale.Earth) |> Chart.show
@@ -60,16 +73,35 @@ let xysizes2 = ["Factor1", "Group1", 1000; "Factor1", "Group2", 3; "Factor2", "G
 Chart.ProportionalArea(xysizes2) |> Chart.show
 let xysizes3 = [1., "Group1", 100; 1., "Group2", 50; 1.1, "Group1", 125; 1.00001, "Group2", 10]
 Chart.ProportionalArea(xysizes3) |> Chart.show
+let xysizes4 = [
+    "Factor1", "Group1", 1000
+    "Factor1", "Group2", 1000
+    "Factor1", "Group3", 1000
+    "Factor1", "Group4", 1000
+    "Factor1", "Group5", 1000
+    "Factor1", "Group6", 1000
+    "Factor1", "Group7", 1000
+    "Factor1", "Group8", 1000
+    "Factor2", "Group1", 1000
+    "Factor2", "Group2", 1000
+    "Factor2", "Group3", 1000
+    "Factor2", "Group4", 1000
+    "Factor2", "Group5", 1000
+    "Factor2", "Group6", 1000
+    "Factor2", "Group7", 1000
+    "Factor2", "Group8", 1000
+    ]
+Chart.ProportionalArea(xysizes4) |> Chart.withSize(1800.,800.) |> Chart.show
 
 
 
-/// Takes an array of floats and performs a 0;1-normalization on it.
-let normalize (data: float []) =
-    let yMax = Array.max data
-    let yMin = Array.min data
-    data |> Array.map (fun y -> (y + 3. - yMin) / (yMax + 3. - yMin) * 175.)
+/// Takes a seq of floats and performs a [`upperBound`;`lowerBound`]-normalization on it.
+let normalize lowerBound upperBound (data: float seq) =
+    let yMax = Seq.max data
+    let yMin = Seq.min data
+    data |> Seq.map (fun y -> (y - yMin) / (yMax - yMin) * (upperBound - lowerBound) + lowerBound)
 
-normalize (xysizes2 |> List.map (fun (p,g,s) -> float s) |> Array.ofList)
+normalize 3. 175. (xysizes2 |> List.map (fun (p,g,s) -> float s))
 
 let testSatz = [4.; 7.; 3.; 5.; 10.]
 let minDavon = List.min testSatz
